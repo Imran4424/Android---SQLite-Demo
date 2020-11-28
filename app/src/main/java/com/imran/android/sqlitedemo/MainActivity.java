5ff8e3356a8cd2eb99ff8a55a25cc98e3f60b6b6
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-28 21:30:51 +0600

    main activity

--- app/src/main/java/com/imran/android/sqlitedemo/MainActivity.java
+++ app/src/main/java/com/imran/android/sqlitedemo/MainActivity.java
@@ -2,7 +2,10 @@ package com.imran.android.sqlitedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
 
+        myDatabase.execSQL("INSERT INTO users (name, age) VALUES('Munir', 28)");
+        myDatabase.execSQL("INSERT INTO users (name, age) VALUES('Imran', 23)");
+        myDatabase.execSQL("INSERT INTO users (name, age) VALUES('Anik', 26)");
 
+        Cursor cursor = myDatabase.rawQuery("SELECT * FROM USERS", null);
 
-        
+        int nameIndex = cursor.getColumnIndex("name");
+        int ageIndex = cursor.getColumnIndex("age");
+
+        cursor.moveToFirst();
+
+        while (cursor != null) {
+            Log.i("name", cursor.getString(nameIndex));
+            Log.i("age", cursor.getString(ageIndex));
+
+            cursor.moveToNext();
+        }
+
     }
 }
